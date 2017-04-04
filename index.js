32d393232da47bc6a1497417705997a563bae88e
Ricky Ng-Adam <rngadam@gmail.com>
2017-04-04 13:45:14 +0800

    make config a parameter

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
-const d = ['a', 'b', 'c', 'd', 'f', 'h', 'k', 'o', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
-const b = 4
-const n = 6
-const m = (2**b)**n+1
-const x = 279554824
-const y = 7560760 // modinv(x, m)
-const mask = (2**b)-1
+function sampleConf() {
+    const d = ['a', 'b', 'c', 'd', 'f', 'h', 'k', 'o', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+    const b = 4
+    const n = 6
+    const m = (2**b)**n+1
+    const x = 279554824
+    const y = 7560760 // modinv(x, m)
+    const mask = (2**b)-1
+    const shifts = [0, 4, 8, 12, 16, 20]
+    return {
+        d,
+        b,
+        n,
+        m,
+        x,
+        y,
+        mask,
+        shifts
+    }
+}
 
-function rhash(n) {
-    return n * x % m
+const c = sampleConf()
+
+function rhash(c, n) {
+    return n * c.x % c.m
 } 
 
-function invrhash(h) {
-    return h * y % m  
+function invrhash(c, h) {
+    return h * c.y % c.m  
 }
 
-function shortcode_from_hash(h) {
-    return [0, 4, 8, 12, 16, 20].map((shift) => d[h >> shift & mask])
+function shortcode_from_hash(c, h) {
+    return c.shifts.map((shift) => c.d[h >> shift & c.mask])
 }
 
-function hash_from_shortcode(shortcode) {
-    return shortcode.reduce((h, l, i) => h |= d.indexOf(l) << i*b, 0)
+function hash_from_shortcode(c, shortcode) {
+    return shortcode.reduce((h, l, i) => h |= c.d.indexOf(l) << i*c.b, 0)
 }
 
-function shortcode(v) {
-    return shortcode_from_hash(rhash(v)).join('')
+function shortcode(c, v) {
+    return shortcode_from_hash(c, rhash(c, v)).join('')
 } 
 
-function value(shortcode) {
-    return invrhash(hash_from_shortcode(shortcode.split('')))
+function value(c, shortcode) {
+    return invrhash(c, hash_from_shortcode(c, shortcode.split('')))
 }
 
 module.exports = {
     shortcode,
-    value
+    value,
+    c
 }
--- test.js
+++ test.js
@@ -1,37 +1,38 @@
 const ufhash = require('./index.js')
 const assert = require('assert')
+const c = ufhash.c
 
 describe('ufhash', () => {
     it('shortcode is length 6', () => {
-        assert.equal(ufhash.shortcode(123456).length, 6)
+        assert.equal(ufhash.shortcode(c, 123456).length, 6)
     })
 
     it('shortcode is alphabetic', () => {
-        assert.equal(/[a-z]{6}/.test(ufhash.shortcode(654321)), true)
+        assert.equal(/[a-z]{6}/.test(ufhash.shortcode(c, 654321)), true)
     })
 
     it('can generate shortcode and reverse it for 0', () => {
-        assert.equal(ufhash.value(ufhash.shortcode(0)), 0)
+        assert.equal(ufhash.value(c, ufhash.shortcode(c, 0)), 0)
     })
 
     it('can generate shortcode and reverse it for 1', () => {
-        assert.equal(ufhash.value(ufhash.shortcode(1)), 1)
+        assert.equal(ufhash.value(c, ufhash.shortcode(c, 1)), 1)
     })
 
     it('can generate shortcode and reverse it for maximum', () => {
-        assert.equal(ufhash.value(ufhash.shortcode(16**6)), 16**6)
+        assert.equal(ufhash.value(c, ufhash.shortcode(c, 16**6)), 16**6)
     })
 
     it('generate unique shortcodes for first 100K', () => {
         var shortcodes = {}
         for(var i=0; i<100000; i++) {
-            var shortcode = ufhash.shortcode(i)
+            var shortcode = ufhash.shortcode(c, i)
             assert(!shortcodes[shortcode])
             shortcodes[shortcode] = true
         }
     })
 
     it('consistent shortcode between calls', () => {
-        assert.equal(ufhash.shortcode(8888888), 'hsbovu')
+        assert.equal(ufhash.shortcode(c, 8888888), 'hsbovu')
     })
 })
